794a73d4a901b4be4e555715519ec471b10b9826
Spirese <jesusguillen545@gmail.com>
2024-03-31 20:51:57 -0600

    AtBSwP start!
    That. Just the introduction and the other projects.

--- Mega Projects/1. Numbers/1. Pi.py
+++ Mega Projects/1. Numbers/1. Pi.py
@@ -2,12 +2,14 @@
 # Enter a number and have the program generate PI up to that many decimal places. 
 # Keep a limit to how far the program will go.
 
-# Inputs
-n = int(input("Introduzca el número de digitos de Pi que quiere mostrar:"))
 
-for i in range(n):
-    R = R + 1/(n+2)
+n = 1000000 # Número de términos en la serie de Leibniz
+pi_aprox = 0 # La variable que va a almacenar el valor de pi
+sign = 1 # Valor del signo para ir alternando
 
-val = 4 * (1 - R)
+# for i in range(inicio, fin [se suma el 1 para contener el valor de n*2], pasos de 2 en 2)
+for i in range(1, n * 2 + 1, 2):
+    pi_aprox += sign * 4 / i
+    sign *= -1
 
-print("El valor de Pi es: %d" %(val))
+print(pi_aprox)
